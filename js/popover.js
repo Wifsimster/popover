f06a6840729542e1468a2ddb40c3ebe755dfdf01
Wifsimster <wifsimster@gmail.com>
2016-02-03 17:58:05 +0100

    Improve positionement

--- js/popover.js
+++ js/popover.js
@@ -4,32 +4,50 @@
 *   Release date:   03/02/2016
 ***************************************/
 (function ($) {
+  "use strict";
   $.fn.popover = function () {
-    $self = $(this);
-    var $container = $('#'+$self.attr('data-popover'));
+    var OFFSET = 15, $container;
+
     // Moved all popover at the  end of body
-    $.each(this, function(index, elt) {
-      $container = $('#'+$(elt).attr('data-popover'));
+    $.each(this, function (index, elt) {
+      $container = $('#' + $(elt).attr('data-popover'));
       $container.clone().appendTo('body');
-      $container[0].remove();
+      if ($container.length > 0) {
+        $container[0].remove();
+      }
     });
-    var OFFSET = 26;
-    $self.on('click', function() {
-      $container = $('#'+$(this).attr('data-popover'));
+
+    $(this).on('click', function () {
+      $container = $('#' + $(this).attr('data-popover'));
       var bodyRect = document.body.getBoundingClientRect(),
           elemRect = this.getBoundingClientRect(),
           leftOffset = elemRect.left - bodyRect.left,
-          topOffset   = elemRect.top - bodyRect.top + OFFSET;
-      if($(this).attr('data-popover-position') === 'top') {
-        $container.css('left', leftOffset - ($container.width() / 2)).css('top', topOffset - $container.height() - (OFFSET * 2)).addClass('show');
-      } else if($(this).attr('data-popover-position') === 'left') {
-        $container.css('left', leftOffset - $container.width() - (OFFSET * 1.2)).css('top', topOffset - ($container.height() / 2) - OFFSET ).addClass('show');
-      }  else if($(this).attr('data-popover-position') === 'right') {
-        $container.css('left', leftOffset + OFFSET).css('top', topOffset - ($container.height() / 2) - OFFSET ).addClass('show');
+          topOffset   = elemRect.top - bodyRect.top,
+          containerWidth = $container.width() + parseInt($container.css('padding-left')) + parseInt($container.css('padding-right')),
+          diff = containerWidth > $(this).width() ? (containerWidth - $(this).width())/2 : ($(this).width() - containerWidth)/2;
+      if ($(this).attr('data-popover-position') === 'left') {
+        $container
+          .css('right', leftOffset + $(this).width() + OFFSET)
+          .css('top', topOffset - ($container.height() / 2))
+          .addClass('show');
+      } else if ($(this).attr('data-popover-position') === 'right') {
+        $container
+          .css('left', leftOffset + $(this).width() + OFFSET)
+          .css('top', topOffset - ($container.height() / 2))
+          .addClass('show');
+      } else if ($(this).attr('data-popover-position') === 'top') {
+        $container
+          .css('left', leftOffset - diff)
+          .css('top', topOffset - $(this).height() - $container.height() - OFFSET)
+          .addClass('show');
       } else {
-        $container.css('left', leftOffset - ($container.width() / 2)).css('top', topOffset).addClass('show');
+        $container
+          .css('left', leftOffset + diff)
+          .css('top', topOffset + $(this).height() + OFFSET)
+          .addClass('show');
       }
     });
+
     $(document).mouseup(function (e) {
       if ($container !== null && !$container.is(e.target) && $container.has(e.target).length === 0) {
         $container.removeClass('show');
